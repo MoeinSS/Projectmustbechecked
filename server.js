203a0f97e2e5862824b0f37777e75e3b3c0cfd0b
MoeinSS <121390376+MoeinSS@users.noreply.github.com>
2023-06-12 15:23:32 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const authRoutes = require('./router/index');
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use('/', authRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
